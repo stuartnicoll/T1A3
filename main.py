0375288b55d73f78d8470ec9f7d41373e05d7453
planetofthecodemonkeys@gmail.com <114849060+stuartnicoll@users.noreply.github.com>
2022-12-11 17:48:43 +1100

    Adding Pseudo outline

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 #This is the main python file for assignment T1A3
+
+#Pseudo code
+#Section 1 The intro
+#make intro section which include read and write to text files.
+#import time 
+#import datetime
+#create intro function 
+#make time delay loop
+#make text read and write to input 
+#convert name to binary output
+#display todays date
+
+#Section 2 the shooping list part
+#Make a function to create shopping list called main menu
+#Make a set of conditional statements
+#Create a variable called shopping list
+#create a function for each selection to perform
+
+#Section 3 the bonus paper rock scissors part
+#this is the bonus game section of the program
+#import random and math functions
+#make input choice for human 
+#and make computer choice random
+#use ceil (n/2) method to create best of n wins
+#use if/than statements to set the conditions
+#and output messages
+
+#Section 4 the bonus match case
+#Choose your desert
+#Make match case function 
+#Escape from program with leaving message.
